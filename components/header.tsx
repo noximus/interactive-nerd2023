22ceb397516b303be5d15cd0353caa8a482dc942
Noximus <hrichardson83@gmail.com>
2023-11-26 18:04:24 -0500

    setup

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,18 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import styles from './header.module.scss';
 
 const Header = () => {
   return (
-    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
-      <Link href="/" className="hover:underline">
-        Blog
-      </Link>
-      .
-    </h2>
-  )
-}
+    <header className={styles.header}>
+      <Link href="/">Blog</Link>
+      <div className={styles.mobileMenu}>
+        <span className="ncs"></span>
+        <span className="nos"></span>
+        <span className="nbs"></span>
+        <div className="menu-button-text">Menu</div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
---
+++ components/layout.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import Header from './header';
+import styles from './layout.module.scss';
+
+const Layout = () => {
+  return (
+    <div className={styles.container}>
+      <Header />
+      <div>Share</div>
+    </div>
+  );
+};
+
+export default Layout;
