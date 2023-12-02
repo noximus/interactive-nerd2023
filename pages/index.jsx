aba0795f7810321d0c9f214d52990f59d86e37e4
Noximus <hrichardson83@gmail.com>
2023-12-02 16:20:55 -0500

    saving progress

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,11 @@
 import Header from '../components/header';
+import MenuModal from '../components/ui/MenuModal';
+import styles from './index.module.scss';
 
 export default function Index() {
   return (
     <>
+      <MenuModal />
       <Header />
       <div>This is where the content will go</div>
     </>
