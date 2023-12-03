c3731baebebaa3dbff67db712abae74c38e26612
Noximus <hrichardson83@gmail.com>
2023-12-03 15:27:33 -0500

    added fix for out animation

--- components/ui/ShareBtn.jsx
+++ components/ui/ShareBtn.jsx
@@ -6,28 +6,49 @@ import ShareModal from './ShareModal';
 
 
 const ShareBtn = () => {
-	const shareModal = useRef();
-	const shareCurrent = shareModal.current
-	const [shareClicked, setShareClicked] = useState(false)
-	const [modalShowing, setModalShowing] = useState(false)
+	const shareBtn = useRef();
+  const shareModal = useRef();
+  const shareBtnCurrent = shareBtn.current;
+  const shareModalCurrent = shareModal.current;
+  const [shareClicked, setShareClicked] = useState(false);
+  const [modalShowing, setModalShowing] = useState(false);
 
-	const handleClick = () => {
-		setShareClicked(!shareClicked)
-	}
-	
-	useEffect(() => {
-		if(shareClicked){
-			gsap.to(shareCurrent, {width:"225px", duration:.5, onComplete:function(){setModalShowing(true)}})
-		}else{
-			gsap.to(shareCurrent, {width:"0px", duration:.5, delay:1, onComplete:setModalShowing(false)})
-		}
-	}, [shareClicked])
+  const handleClick = () => {
+    setShareClicked(!shareClicked);
+  };
+  const handleMouseEnter = () => {
+    gsap.to(shareBtnCurrent, { color: '#F57500', duration: 0.2 });
+  };
+  const handleMouseLeave = () => {
+    gsap.to(shareBtnCurrent, { color: '#FFFFFF', duration: 0.2 });
+  };
 
-	return (
-		<div className={styles.share} onClick={handleClick}>Share
-			<ShareModal ref={shareModal} animate={modalShowing}/>
-		</div>
-	)
+  useEffect(() => {
+    if (shareClicked) {
+      gsap.to(shareBtnCurrent, {
+        backgroundColor: '#222222',
+        duration: 0.5,
+      });
+      gsap.to(shareModalCurrent, {
+        width: '225px',
+        duration: 0.5,
+        onComplete: function () {
+          setModalShowing(true);
+        },
+      });
+    } else {
+      gsap.to(shareBtnCurrent, { backgroundColor: '#F57500', duration: 0.5 });
+      gsap.to(shareModalCurrent, { width: '0px', duration: 0.5, delay: 1, onComplete: setModalShowing(false) });
+    }
+  }, [shareClicked]);
+
+  return (
+    <div ref={shareBtn} className={styles.share} onClick={handleClick} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      Share
+      <div className={styles.shareBtnOverLayer}></div>
+      <ShareModal ref={shareModal} animate={modalShowing} />
+    </div>
+  );
 };
 
 export default ShareBtn;
--- components/ui/ShareModal.jsx
+++ components/ui/ShareModal.jsx
@@ -1,49 +1,55 @@
 import { useEffect, useRef, forwardRef } from 'react';
 import { gsap } from 'gsap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook,faTwitter,faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faGithubAlt, faFacebook, faInstagram, faLinkedinIn, faDiscord } from '@fortawesome/free-brands-svg-icons';
+import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons';
 import styles from './ShareModal.module.scss';
 
 const ShareModal = forwardRef((props, ref) => {
-  const socialIcon1 = useRef()
-  const socialIcon2 = useRef()
-  const socialIcon3 = useRef()
-  const socialIcon4 = useRef()
-  const socialIcon5 = useRef()
-  const allIcons = [socialIcon1.current,socialIcon2.current,socialIcon3.current,socialIcon4.current,socialIcon5.current]
+  const socialIcon1 = useRef();
+  const socialIcon2 = useRef();
+  const socialIcon3 = useRef();
+  const socialIcon4 = useRef();
+  const socialIcon5 = useRef();
+  const returnIcon = useRef();
+  const getAllIcons = () => [socialIcon1.current, socialIcon2.current, socialIcon3.current, socialIcon4.current, socialIcon5.current, returnIcon.current];
 
   useEffect(() => {
-		if(props.animate){
-			gsap.to(allIcons, {duration:1, stagger:.1, ease:"out", opacity:1})
-		}else{
-			// gsap.to(allIcons, {duration:1, ease:"out", opacity: 0})
-		}
-	}, [props.animate])
+    const allIcons = getAllIcons();
+    if (props.animate) {
+      gsap.to(allIcons, { duration: 1, stagger: 0.1, ease: 'inOut', opacity: 1 });
+    } else if (!props.animate) {
+      gsap.to(allIcons, { duration: 1, stagger: 0.05, ease: 'inOut', opacity: 0 });
+    }
+  }, [props.animate]);
 
   return (
     <div ref={ref} className={styles.shareModal}>
       <button ref={socialIcon1} className={styles.socialBtns}>
-        <FontAwesomeIcon icon={faFacebook} className={styles.socialIcons}/>
+        <FontAwesomeIcon icon={faGithubAlt} className={styles.socialIcons} />
       </button>
 
       <button ref={socialIcon2} className={styles.socialBtns}>
-        <FontAwesomeIcon icon={faTwitter} className={styles.socialIcons}/>
+        <FontAwesomeIcon icon={faInstagram} className={styles.socialIcons} />
       </button>
-      
+
       <button ref={socialIcon3} className={styles.socialBtns}>
-        <FontAwesomeIcon icon={faLinkedinIn} className={styles.socialIcons}/>
+        <FontAwesomeIcon icon={faLinkedinIn} className={styles.socialIcons} />
       </button>
 
       <button ref={socialIcon4} className={styles.socialBtns}>
-        <FontAwesomeIcon icon={faLinkedinIn} className={styles.socialIcons}/>
+        <FontAwesomeIcon icon={faFacebook} className={styles.socialIcons} />
       </button>
-      
+
       <button ref={socialIcon5} className={styles.socialBtns}>
-        <FontAwesomeIcon icon={faLinkedinIn} className={styles.socialIcons}/>
+        <FontAwesomeIcon icon={faDiscord} className={styles.socialIcons} />
+      </button>
+
+      <button ref={returnIcon} className={styles.returnBtn}>
+        <FontAwesomeIcon icon={faArrowLeftLong} className={styles.returnIcon} />
       </button>
     </div>
-
-  )
-})
+  );
+});
 
 export default ShareModal;
