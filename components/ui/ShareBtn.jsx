69111682ceffb6544ffeba83f260e961e598b9d1
Noximus <hrichardson83@gmail.com>
2023-12-04 00:45:12 -0500

    redux added

--- components/ui/ShareBtn.jsx
+++ components/ui/ShareBtn.jsx
@@ -1,14 +1,17 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, useLayoutEffect, useContext } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { gsap } from 'gsap';
 import styles from './ShareBtn.module.scss';
 import ShareModal from './ShareModal';
 import { set } from 'date-fns';
 
 const ShareBtn = () => {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.myReducer.value);
   const shareBtn = useRef();
   const shareBtnOverlay = useRef();
   const shareModal = useRef();
-	const tween = useRef(null);
+  const tween = useRef(null);
   const shareBtnCurrent = shareBtn.current;
   const shareBtnOverlayCurrent = shareBtnOverlay.current;
   const shareModalCurrent = shareModal.current;
@@ -26,7 +29,7 @@ const ShareBtn = () => {
     setShareBtnHover(false);
   };
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (shareBtnHover) {
       gsap.to(shareBtnCurrent, { color: '#F57500', duration: 0.2 });
       gsap.to(shareBtnOverlayCurrent, { top: '-80px', ease: 'inOut', duration: 0.2 });
@@ -35,7 +38,7 @@ const ShareBtn = () => {
       gsap.to(shareBtnOverlayCurrent, { top: '0', ease: 'inOut', duration: 0.2 });
     }
   }, [shareBtnHover]);
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (shareClicked) {
       tween.current = gsap.to(shareModalCurrent, {
         right: '0',
@@ -52,7 +55,7 @@ const ShareBtn = () => {
         tween.current = gsap.to(shareModalCurrent, { right: '-240px', ease: 'inOut', duration: 0.5 });
         gsap.to(shareBtnOverlayCurrent, { backgroundColor: '#F57500', duration: 0 });
       } else {
-        tween.current = gsap.to(shareModalCurrent, { right: '-240px', ease: 'inOut', duration: 0.5, delay: 1, onComplete: () => setModalShowing(false) });
+        tween.current = gsap.to(shareModalCurrent, { right: '-240px', ease: 'inOut', duration: 0.5, delay: 2, onComplete: () => setModalShowing(false) });
         gsap.to(shareBtnOverlayCurrent, { backgroundColor: '#F57500', duration: 0 });
       }
     }
@@ -61,7 +64,7 @@ const ShareBtn = () => {
   return (
     <div className={styles.share}>
       <span ref={shareBtn} className={styles.shareText} onClick={handleClick} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-        Share
+        share {count}
       </span>
       <div ref={shareBtnOverlay} className={styles.shareBtnOverlay}></div>
       <ShareModal ref={shareModal} animate={modalShowing} />
